a59e893c828657433e93e9b1c64c7eca68130972
ShreyaAgrawal-jp <63266009+ShreyaAgrawal-jp@users.noreply.github.com>
2021-02-13 17:16:12 +0530

    Add files via upload

---
+++ fibonacci2.py
@@ -0,0 +1,11 @@
+#fibonacci using recursive function
+def fib(n):
+    if n<=1:
+        return n
+    else:
+        return (fib(n-1)+fib(n-2))
+n=int(input('enter a number: '))
+for i in range(n):
+    print(fib(i),end='')
+
+
